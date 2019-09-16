f9b46e9839baabb19ef48e47df0cf1645ef609b9
tjilani <tariq.jilani@protonmail.com>
2019-09-16 21:57:03 +0200

    heroku database URL added

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const image = require('./controllers/image');
 const postgresDB = knex({
   client: "pg",
   connection: {
-    host: "127.0.0.1",
+    host: "postgresql-fluffy-08153",
     user: "postgres",
     password: "bling",
     database: "smartbrain"
